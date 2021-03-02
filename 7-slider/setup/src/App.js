44c48b0e9bd939b70edc5b1b04f6afaa79445424
Maxwell King <maxwelltking@gmail.com>
2021-03-01 22:45:46 -0700

    changed CSS and used useEffect to mimic how project is done in tutorial

--- 7-slider/setup/src/App.js
+++ 7-slider/setup/src/App.js
@@ -7,22 +7,22 @@ function App() {
     const [index, setIndex] = useState(0)
     const [reviews, setReviews] = useState(data)
 
-    const {name, id, title, quote, image} = reviews[index]
-
-    const nextReview = () => {
-        if (index === reviews.length-1){
+    useEffect(() => {
+        const lastIndex = reviews.length - 1;
+        if (index < 0) {
+            setIndex(lastIndex)
+        }
+        if (index > lastIndex){
             setIndex(0)
-        }else {
-            setIndex(index+1)
         }
-    }
-    const prevReview = () => {
-        if (index === 0){
-            setIndex(reviews.length-1)
-        }else {
-            setIndex(index-1)
-        }
-    }
+    }, [index, reviews])
+
+    useEffect( () => {
+        let slider = setInterval( () => {setIndex(index +1)}, 3000);
+        return () => clearInterval(slider)
+
+    }, [index])
+
 
     return (
         <section className="section">
@@ -31,18 +31,31 @@ function App() {
             </div>
 
             <div className="section-center">
+                {
+                    reviews.map((person, personIndex) => {
+                        const {name, id, title, quote, image} = reviews[personIndex]
 
-                <article key={id} className="">
-                    <img className="person-img" src={image} alt={name}/>
-                    <h4>{name}</h4>
-                    <p className="title">{title}</p>
-                    <p className="text">{quote}</p>
-                    <FaQuoteRight className="icon"/>
-                </article>
-                <button className="prev" onClick={() => prevReview()}>
+                        let position = 'nextSlide';
+
+                        personIndex === index ? position = 'activeSlide' : position = position;
+                        personIndex === index - 1 || (index == 0 && personIndex === reviews.length - 1) ? position = 'lastSlide' : position = position;
+                        return (
+                            <article key={id} className={position}>
+                                <img className="person-img" src={image} alt={name}/>
+                                <h4>{name}</h4>
+                                <p className="title">{title}</p>
+                                <p className="text">{quote}</p>
+                                <FaQuoteRight className="icon"/>
+                            </article>
+                        );
+                    })
+                }
+
+
+                <button className="prev" onClick={() => setIndex(index - 1)}>
                     <FiChevronLeft/>
                 </button>
-                <button className="next" onClick={() => nextReview()}>
+                <button className="next" onClick={() => setIndex(index + 1)}>
                     <FiChevronRight/>
                 </button>
 
